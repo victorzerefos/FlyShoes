e73601dfa0889ba21850491efb39e1a97bbfda5f
Victor Zerefos <victorzerefos@icloud.com>
2021-07-02 21:35:59 -0300

    Initial Commit

---
+++ FlyShoes/FlyShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyShoesApp.swift
+//  FlyShoes
+//
+//  Created by Victor Zerefos on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct FlyShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
